2dc41bf50f92d40dc3a374ef82b079e73524c420
rijan <hrijon.chp@gmail.com>
2020-03-13 16:54:43 +0800

    url and querystring modules

---
+++ lab3/server-ex5.js
@@ -0,0 +1,23 @@
+var http = require("http"); // import http core modules
+var url = require("url"); // import url core modules
+var qs = require('querystring'); //import querystring 
+
+http.createServer( function (request, response) {
+      //        use url module to get pathname of requested resource 
+        //  var path = url.parse(request.url).pathname;
+
+        var qstr = url.parse(request.url).query;
+        var path = url.parse(request.url).pathname;
+        var a = qs.parse(qstr)['foo'];
+        var b = qs.parse(qstr)['hello'];
+
+        console.log("Request for " + path + " received.");
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.write(qstr + "\n");
+        response.write(path + "\n");
+        response.write(a + "\n" + b + "\n");
+
+        response.end();
+        }).listen(40310,'localhost');
+        console.log("Server has started.");

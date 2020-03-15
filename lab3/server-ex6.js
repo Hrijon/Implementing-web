3a83b487ce6b5f1ea16d83b3d8d4bfd5945a3361
rijan <hrijon.chp@gmail.com>
2020-03-15 12:07:54 +0800

    reading file using fs core module

---
+++ lab3/server-ex6.js
@@ -0,0 +1,28 @@
+var http = require("http"); // import http core modules
+var url = require("url"); // import url core modules
+const fs = require('fs');//import fs filesystem
+
+http.createServer( function (request, response) {
+      //        use url module to get pathname of requested resource 
+        //  var path = url.parse(request.url).pathname;
+
+        var path = url.parse(request.url).pathname;
+
+        //slash nikaleny]
+        const myURL = new URL("http://localhost:40310/startServer")
+
+        console.log("Request for " + path + " received.");
+
+        response.writeHead(200, {"Content-Type": "text/plain"});
+       
+
+      fs.readFile("." + path, 'utf8', (err, data) => {
+            if (err) throw err;
+            console.log(data);
+          });
+
+        response.end();
+        }).listen(40310,'localhost');
+        console.log("Server has started.");
+
+        

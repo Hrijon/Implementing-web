e3bb57893c836bbdb2775d991eb7b07cb5033af0
rijan <hrijon.chp@gmail.com>
2020-03-18 18:13:27 +0800

    islotate string & display hostname:port on server

--- lab3/server-ex4.js
+++ lab3/server-ex4.js
@@ -1,18 +1,24 @@
 var http = require("http"); // import http core modules
 var url = require("url"); // import url core modules
 var qs = require('querystring'); //import querystring 
+
 http.createServer( function (request, response) {
       //        use url module to get pathname of requested resource 
          var pathname = url.parse(request.url).pathname;
 
          console.log("Request for " + pathname + " received.");
-         console.log(qs.stringify(request.headers)); //display header information sent by client
+      //    console.log(qs.stringify(request.headers)); //display header information sent by client
 
          response.writeHead(200, {"Content-Type": "text/plain"});
          response.write("Hello World");
          response.end();
+
+         var string = qs.stringify(request.headers);
+         var stringisyObject = qs.parse(string);
+         var host = stringisyObject["host"];
+         var port = stringisyObject["port"];
+     
+         console.log("Host:Port = " + host);
+
          }).listen(40310,'localhost');
          console.log("Server has started.");
-
-
-

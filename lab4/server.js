7b032a05ae24eea76b7ccf2ebc1ced4452ce925a
rijan <hrijon.chp@gmail.com>
2020-03-21 19:14:45 +0800

    Fixed bug port number and localhost

--- lab4/server.js
+++ lab4/server.js
@@ -1,16 +1,19 @@
-var http = require("http");
-var url = require("url");
-9
-// import http core modules
-// import url core modules
-function startServer(route){
+var http = require("http"); // import http core modules
+var url = require("url"); // import url core modules
+function startServer(route)
+{
       http.createServer( function (request, response) {
+            //        use url module to get pathname of requested resource 
             var pathname = url.parse(request.url).pathname;
             route(pathname);
+
+            console.log("Request for " + pathname + " received.");
             response.writeHead(200, {"Content-Type": "text/plain"});
             response.write("Hello World");
             response.end();
-            }).listen(88888);
+            }).listen(40310,'localhost');
             console.log("Server has started.");
+
 }
+
 exports.startServer = startServer;

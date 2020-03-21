f0f0b5b3b2717192e3aa198a235a33a00eb7c35e
rijan <hrijon.chp@gmail.com>
2020-03-21 18:47:35 +0800

    router for request handler

---
+++ lab4/router.js
@@ -0,0 +1,6 @@
+// create route function with pathname as parameter
+function route(pathname) {
+    console.log("Routing a request for " + pathname);
+    }
+    // export route function
+    exports.route = route;

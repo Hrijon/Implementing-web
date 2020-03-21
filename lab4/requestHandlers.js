7a6d0a3ea77c5885aed48af9dff0df0a1722a9d7
rijan <hrijon.chp@gmail.com>
2020-03-21 21:58:08 +0800

    exec response

--- lab4/requestHandlers.js
+++ lab4/requestHandlers.js
@@ -1,9 +1,21 @@
-function reqStart(){
+var exec = require("child_process").exec;
+
+function reqStart(response){
     console.log("Request handler 'start' was called.");
+    exec("ls -lah",
+        function (error, stdout, stderr) {
+        response.writeHead( 200, {"Content-Type": "text/plain"} );
+        response.write(stdout);
+        response.end();
+    });
 }
 
-function reqUpload(){
+function reqUpload(response){
     console.log("Request handler 'upload' was called.");
+
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("success in upload");
+    response.end();
 }
 
 exports.reqStart = reqStart;

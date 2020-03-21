9651c8a2b7672f499485029217383104f6de8277
rijan <hrijon.chp@gmail.com>
2020-03-21 21:56:39 +0800

    passing response arg from server and receiving in router

--- lab4/router.js
+++ lab4/router.js
@@ -1,12 +1,15 @@
-function route(pathname, handle) {
+function route(pathname, handle, response) {
+                
     // note access via associative array notation
     // if the path points to a function i.e. request handler
     if (typeof handle[pathname] === 'function') {
-    handle[pathname](); // call the appropriate function
+        handle[pathname](response); // call the appropriate function && pass response argument
     } 
-    
     else {
-    console.log("No handler found for: " + pathname);
+        console.log("No handler found for: " + pathname);
+        response.writeHead(404, {"Content-Type": "text/plain"});
+        response.write("Resource not found!");
+        response.end();
     }
     }
     exports.route = route;
--- lab4/server.js
+++ lab4/server.js
@@ -6,12 +6,10 @@ function startServer(route, handle) //route: function, handle:object in para
       {
             //        use url module to get pathname of requested resource 
             var pathname = url.parse(request.url).pathname;
-            route(pathname, handle);
+            route(pathname, handle, response);
 
             console.log("Request for " + pathname + " received.");
-            response.writeHead(200, {"Content-Type": "text/plain"});
-            response.write("Hello World");
-            response.end();
+            
       }).listen(40310,'localhost');
             console.log("Server has started.");
             
